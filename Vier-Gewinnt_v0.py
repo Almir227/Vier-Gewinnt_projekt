529ab6d59a54ae951be54014fb2a7a37c1ab35b7
Almir227 <almir.saitoski@gmail.com>
2026-03-17 15:54:42 +0100

    Modell des Spiels wurde umgesetzt (refs #2)

---
+++ Vier-Gewinnt_v0.py
@@ -0,0 +1,46 @@
+import pygame
+pygame.init()
+
+WHITE = (255,255,255)
+
+screen = pygame.display.set_mode((390,390))
+pygame.display.set_caption("VIERGEWINNT_2.py")
+
+s6 = [  "0" ]*7
+s5 = [  "0" ]*7
+s4 = [  "0" ]*7
+s3 = [  "0" ]*7
+s2 = [  "0" ]*7
+s1 = [  "0" ]*7
+s0 = [  "0" ]*7
+ 
+feld = [s0,s1,s2,s3,s4,s5,s6]
+
+#Für Weiterarbeit: 1 Dimension ist Spalte, die andere Zeile, immer "T" oder "X" für die 2 Spieler oder 0 für Leer
+
+aktiv = True
+
+while aktiv:
+    pygame.draw.line(screen, WHITE, (20,20), (370,20) )
+    pygame.draw.line(screen, WHITE, (20,70), (370,70) )
+    pygame.draw.line(screen, WHITE, (20,120), (370,120) )
+    pygame.draw.line(screen, WHITE, (20,170), (370,170) )
+    pygame.draw.line(screen, WHITE, (20,220), (370,220) )
+    pygame.draw.line(screen, WHITE, (20,270), (370,270) )
+    pygame.draw.line(screen, WHITE, (20,320), (370,320) )
+    pygame.draw.line(screen, WHITE, (20,370), (370,370) )
+   
+    pygame.draw.line(screen, WHITE, (20,20), (20, 370) )
+    pygame.draw.line(screen, WHITE, (70,20), (70,370) )
+    pygame.draw.line(screen, WHITE, (120,20), (120,370) )
+    pygame.draw.line(screen, WHITE, (170,20), (170,370) )
+    pygame.draw.line(screen, WHITE, (220,20), (220,370) )
+    pygame.draw.line(screen, WHITE, (270,20), (270,370) )
+    pygame.draw.line(screen, WHITE, (320,20), (320,370) )
+    pygame.draw.line(screen, WHITE, (370,20), (370,370) )
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        pygame.event.get()
+        if event.type == pygame.QUIT:
+            aktiv = False
